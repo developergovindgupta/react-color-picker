d3bac79f8a02e37fb11b85787528fc4bb8d161e2
developergovindgupta <govindprasadguptamca@gmail.com>
2022-02-09 22:40:55 +0530

    Updates

---
+++ precache-manifest.a874ab2179957fd8d139a86a96ed16b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0e79a0ded3ce3ee7d3f5f66b1e8831b",
+    "url": "/react-color-picker/index.html"
+  },
+  {
+    "revision": "75d1b32789215dc536e3",
+    "url": "/react-color-picker/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5caeaae02598237feb23",
+    "url": "/react-color-picker/static/js/2.ea490775.chunk.js"
+  },
+  {
+    "revision": "bd81e5c6feaf63e9a711",
+    "url": "/react-color-picker/static/js/3.4183067e.chunk.js"
+  },
+  {
+    "revision": "75d1b32789215dc536e3",
+    "url": "/react-color-picker/static/js/main.f580c767.chunk.js"
+  },
+  {
+    "revision": "e416603048a2407d4fc1",
+    "url": "/react-color-picker/static/js/runtime~main.41a5ed0e.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/react-color-picker/static/media/logo.06e73328.svg"
+  }
+]);
