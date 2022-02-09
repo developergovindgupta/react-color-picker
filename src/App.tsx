45001feb8434c71201dc3c90bb2f1660fd0410c8
developergovindgupta <govindprasadguptamca@gmail.com>
2022-02-10 00:21:58 +0530

    add scss watch for typescript

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <h3>react-color-picker</h3>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/developergovindgupta/react-color-picker"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         view source
         </a>
       </header>
     </div>
